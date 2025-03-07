d6de0efd9b5776987bb927d10ce7d48f22c91d6f
peizong <45988544+peizong@users.noreply.github.com>
2025-03-07 11:58:03 -0500

    Update streamlit_app_cloud.py

--- LLM_UI/streamlit_app_cloud.py
+++ LLM_UI/streamlit_app_cloud.py
@@ -62,7 +62,7 @@ class FaissEmbedder:
             base_url="https://ai-gateway.apps.cloud.rt.nyu.edu/v1/", #PORTKEY_GATEWAY_URL,
             default_headers=createHeaders(
             provider="openai",
-            api_key= os.environ.get("PORTKEY_API_KEY")， #"PORTKEY_API_KEY", # defaults to os.environ.get("PORTKEY_API_KEY"),
+            api_key= os.environ.get("PORTKEY_API_KEY"), #"PORTKEY_API_KEY", # defaults to os.environ.get("PORTKEY_API_KEY"),
             virtual_key= os.environ.get("VIRTUAL_KEY_VALUE") #if you want provider key on gateway instead of client
                                           )
         )
