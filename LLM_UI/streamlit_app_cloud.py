0cacad2226e1d229e75fc84ce4e6d28913c00d4e
peizong <45988544+peizong@users.noreply.github.com>
2025-03-25 17:02:56 -0400

    Update streamlit_app_cloud.py

--- LLM_UI/streamlit_app_cloud.py
+++ LLM_UI/streamlit_app_cloud.py
@@ -67,7 +67,7 @@ class FaissEmbedder:
         #                                   )
         # )
         self.openai_client = OpenAI(
-            #api_key="xxx", #"OPENAI_API_KEY", # defaults to os.environ.get("OPENAI_API_KEY")
+            api_key="xxx", #"OPENAI_API_KEY", # defaults to os.environ.get("OPENAI_API_KEY")
             base_url="https://ai-gateway.apps.cloud.rt.nyu.edu/v1/", #PORTKEY_GATEWAY_URL,
             default_headers=createHeaders(
             #provider="openai",
